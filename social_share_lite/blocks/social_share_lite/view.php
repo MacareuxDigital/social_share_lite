b0b13c06c336aaa61acb9fd98d89c5b161dfec07
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2014-07-20 09:13:30 +0900

    Fixes #2 #5

--- social_share_lite/blocks/social_share_lite/view.php
+++ social_share_lite/blocks/social_share_lite/view.php
@@ -1,22 +1,21 @@
 <?php  defined('C5_EXECUTE') or die("Access Denied.");
-$nh = Loader::helper('navigation');
-$th = Loader::helper('text');
-$page = Page::getCurrentPage();
-$url = $nh->getLinkToCollection($page);
 
 if($page->isEditMode()) { ?>
 <div class="ccm-edit-mode-disabled-item"><?php echo t('Social share buttons disabled in edit mode.');?></div>
 <?php
 } else {
 
-echo '<div class="social-share">';
+echo '<div class="ccm-social-share">';
+echo '<ul>';
 
 /**
  * Like Button from Facebook
  * get another code: https://developers.facebook.com/docs/reference/plugins/like/
  */
 if($fblike){ ?>
-<div class="fb-like" data-href="<?php echo BASE_URL . $th->specialchars($url); ?>" data-send="false" data-layout="button_count" data-width="120" data-show-faces="false"></div><?php
+<li class="fblike">
+<div class="fb-like" data-href="<?php echo $th->specialchars($url); ?>" data-send="false" data-layout="button_count" data-width="120" data-show-faces="false"></div>
+</li><?php
 }
 
 /**
@@ -24,7 +23,9 @@ if($fblike){ ?>
  * get another code: https://dev.twitter.com/docs/tweet-button
  */
 if($tweet){ ?>
-<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo BASE_URL . $th->specialchars($url); ?>" data-lang="<?php echo LANGUAGE; ?>"<?php if(isset($twitter_site) && !empty($twitter_site)){?> data-via="<?php echo $th->specialchars($twitter_site); ?>"<?php }?>><?php echo t('Tweet'); ?></a><?php
+<li class="tweet">
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo $th->specialchars($url); ?>" data-lang="<?php echo $th->specialchars($language); ?>"<?php if(isset($twitter_site) && !empty($twitter_site)){?> data-via="<?php echo $th->specialchars($twitter_site); ?>"<?php }?>><?php echo t('Tweet'); ?></a>
+</li><?php
 }
 
 /**
@@ -32,7 +33,9 @@ if($tweet){ ?>
  * get another code: https://developers.google.com/+/web/+1button/
  */
 if($gplus){ ?>
-<div class="g-plusone" data-size="medium" data-href="<?php echo BASE_URL . $th->specialchars($url); ?>"></div><?php
+<li class="gplus">
+<div class="g-plusone" data-size="medium" data-href="<?php echo $th->specialchars($url); ?>"></div>
+</li><?php
 }
 
 /**
@@ -40,7 +43,9 @@ if($gplus){ ?>
  * get another code: http://b.hatena.ne.jp/guide/bbutton
  */
 if($bhatena){ ?>
-<a href="http://b.hatena.ne.jp/entry/<?php echo BASE_URL . $th->specialchars($url); ?>" class="hatena-bookmark-button" data-hatena-bookmark-layout="simple-balloon" data-hatena-bookmark-lang="<?php echo LANGUAGE; ?>" title="<?php echo t('Add this entry to hatena bookmark');?>"><img src="http://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="<?php echo t('Add this entry to hatena bookmark');?>" width="20" height="20" style="border: none;" /></a><?php
+<li class="bhatena">
+<a href="http://b.hatena.ne.jp/entry/<?php echo $th->specialchars($url); ?>" class="hatena-bookmark-button" data-hatena-bookmark-layout="simple-balloon" data-hatena-bookmark-lang="<?php echo $th->specialchars($language); ?>" title="<?php echo t('Add this entry to hatena bookmark');?>"><img src="http://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="<?php echo t('Add this entry to hatena bookmark');?>" width="20" height="20" style="border: none;" /></a>
+</li><?php
 }
 
 /**
@@ -48,7 +53,9 @@ if($bhatena){ ?>
  * get another code: http://www.tumblr.com/buttons
  */
 if($tumblr){ ?>
-<a href="http://www.tumblr.com/share" title="<?php echo t('Share on Tumblr'); ?>" class="tumblr-button"><?php echo t('Share on Tumblr'); ?></a><?php
+<li class="tumblr">
+<a href="http://www.tumblr.com/share" title="<?php echo t('Share on Tumblr'); ?>" class="tumblr-button"><?php echo t('Share on Tumblr'); ?></a>
+</li><?php
 }
 
 /**
@@ -56,7 +63,9 @@ if($tumblr){ ?>
  * get another code: http://business.pinterest.com/widget-builder/#do_pin_it_button
  */
 if($pinterest){ ?>
-<a href="//pinterest.com/pin/create/button/" class="pin-it-button" data-pin-do="buttonBookmark" ><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a><span class="social-share-spacer"></span><?php
+<li class="pinterest">
+<a href="//pinterest.com/pin/create/button/" class="pin-it-button" data-pin-do="buttonBookmark" ><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a>
+</li><?php
 }
 
 /**
@@ -64,7 +73,9 @@ if($pinterest){ ?>
  * get another code: http://developer.linkedin.com/plugins/share-plugin-generator
  */
 if($linkedin){ ?>
-<script type="IN/Share" data-url="<?php echo BASE_URL . $th->specialchars($url); ?>" data-counter="right"></script><?php
+<li class="linkedin">
+<script type="IN/Share" data-url="<?php echo $th->specialchars($url); ?>" data-counter="right"></script>
+</li><?php
 }
 
 /**
@@ -72,7 +83,9 @@ if($linkedin){ ?>
  * get another code: http://getpocket.com/publisher/button
  */
 if($pocket){ ?>
-<a data-pocket-label="pocket" data-pocket-count="horizontal" class="pocket-btn" data-lang="<?php echo LANGUAGE; ?>"></a><?php
+<li class="pocket">
+<a data-pocket-label="pocket" data-pocket-count="horizontal" data-save-url="<?php echo $th->specialchars($url); ?>" class="pocket-btn" data-lang="<?php echo $th->specialchars($language); ?>"></a>
+</li><?php
 }
 
 /**
@@ -80,9 +93,12 @@ if($pocket){ ?>
  * get another code: http://media.line.me/howto/en/
  */
 if($line){ ?>
-<span class="line"><script type="text/javascript" src="//media.line.me/js/line-button.js?v=20140127" ></script><script type="text/javascript">new media_line_me.LineButton({"pc":false,"lang":"ja","type":"a"});</script></span><?php
+<li class="line">
+<span><script type="text/javascript">new media_line_me.LineButton({"pc":false,"lang":"<?php echo $th->specialchars($language); ?>","type":"a","text":"<?php echo $th->specialchars($url); ?>","withUrl":true});</script></span>
+</li><?php
 }
 
+echo '</ul>';
 echo '</div>';
 
 }
