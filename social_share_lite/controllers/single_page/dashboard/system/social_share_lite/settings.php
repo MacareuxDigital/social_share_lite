f6f718249fd7467acb957becf908effac913afb6
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2014-12-04 04:50:18 +0900

    PSR-2 again

--- social_share_lite/controllers/single_page/dashboard/system/social_share_lite/settings.php
+++ social_share_lite/controllers/single_page/dashboard/system/social_share_lite/settings.php
@@ -4,30 +4,33 @@ namespace Concrete\Package\SocialShareLite\Controller\SinglePage\Dashboard\Syste
 use \Concrete\Core\Page\Controller\DashboardPageController;
 use Package;
 
-class Settings extends DashboardPageController {
-	
-	public function view() {
-		$pkg = Package::getByHandle('social_share_lite');
-		$disable_scripts = $pkg->getConfig()->get('concrete.sharing.disable_scripts');
-		$this->set('disable_scripts', $disable_scripts);
-	}
+class Settings extends DashboardPageController
+{    
+    public function view()
+    {
+        $pkg = Package::getByHandle('social_share_lite');
+        $disable_scripts = $pkg->getConfig()->get('concrete.sharing.disable_scripts');
+        $this->set('disable_scripts', $disable_scripts);
+    }
 
-	public function updated() {
-		$this->set('message', t("Settings saved."));	
-		$this->view();
-	}
-	
-	public function save_settings() {
-		if ($this->token->validate("save_settings")) {
-			if ($this->isPost()) {
-				$disable_scripts = $this->post('disable_scripts');
-				$pkg = Package::getByHandle('social_share_lite');
-				$pkg->getConfig()->save('concrete.sharing.disable_scripts', $disable_scripts);
-				$this->redirect('/dashboard/system/social_share_lite/settings','updated');
-			}
-		} else {
-			$this->set('error', array($this->token->getErrorMessage()));
-		}
-	}
-	
+    public function updated()
+    {
+        $this->set('message', t("Settings saved."));
+        $this->view();
+    }
+    
+    public function save_settings()
+    {
+        if ($this->token->validate("save_settings")) {
+            if ($this->isPost()) {
+                $disable_scripts = $this->post('disable_scripts');
+                $pkg = Package::getByHandle('social_share_lite');
+                $pkg->getConfig()->save('concrete.sharing.disable_scripts', $disable_scripts);
+                $this->redirect('/dashboard/system/social_share_lite/settings','updated');
+            }
+        } else {
+            $this->set('error', array($this->token->getErrorMessage()));
+        }
+    }
+    
 }
