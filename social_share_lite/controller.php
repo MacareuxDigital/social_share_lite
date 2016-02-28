bf00435b1018129b9845b6d50bf1217d173cbb7d
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2016-02-28 23:45:22 +0900

    Update package version

--- social_share_lite/controller.php
+++ social_share_lite/controller.php
@@ -9,7 +9,7 @@ class Controller extends \Concrete\Core\Package\Package
 {
     protected $pkgHandle = 'social_share_lite';
     protected $appVersionRequired = '5.7.2.1';
-    protected $pkgVersion = '2.1.1';
+    protected $pkgVersion = '2.1.2';
     
     public function getPackageDescription()
     {
