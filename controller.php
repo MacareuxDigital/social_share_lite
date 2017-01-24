e16c0d7fdf6eb2ea6fc5189a823e6969a0e859eb
deek87 <deek87@gmail.com>
2017-01-24 16:39:34 +0000

    Updated Facebook SDK, version number and settings page

--- controller.php
+++ controller.php
@@ -9,7 +9,7 @@ class Controller extends \Concrete\Core\Package\Package
 {
     protected $pkgHandle = 'social_share_lite';
     protected $appVersionRequired = '5.7.2.1';
-    protected $pkgVersion = '2.1.3';
+    protected $pkgVersion = '2.1.4';
     
     public function getPackageDescription()
     {
--- single_pages/dashboard/system/social_share_lite/settings.php
+++ single_pages/dashboard/system/social_share_lite/settings.php
@@ -6,16 +6,20 @@
 
 	<fieldset>
 		<legend><?php echo t('Load scripts')?></legend>
-		<div class="form-group">
+		<div class="control-group">
 			<div class="controls">
-				<label>
-					<?php echo $form->radio('disable_scripts', 0, $disable_scripts); ?>
-					<?php echo t('Add scripts if social share lite blocks are placed on the particular page (default).') ?>
-				</label>
-				<label>
-					<?php echo $form->radio('disable_scripts', 1, $disable_scripts); ?>
-					<?php echo t('Do not add scripts of social network api automatically.') ?>
-				</label>
+				<div class="radio">
+					<label>
+						<?php echo $form->radio('disable_scripts', 0, $disable_scripts); ?>
+						<span><?php echo t('Add scripts if social share lite blocks are placed on the particular page (default).') ?></span>
+					</label>
+				</div>
+				<div class="radio">
+					<label>
+						<?php echo $form->radio('disable_scripts', 1, $disable_scripts); ?>
+						<span><?php echo t('Do not add scripts of social network api automatically.') ?></span>
+					</label>
+				</div>
 				<span class="help-block"><?php echo t("If you want to load javascript files (Twitter widgets.js or Google plusone.js) into your theme manually, please choose second option."); ?></span>
 			</div>
 		</div>
